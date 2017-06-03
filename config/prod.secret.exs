fef9d7344f4a9a7b9658762d1355e07717ae25b7
Horace He <horacehe2007@yahoo.com>
2017-06-03 01:02:33 -0700

    uncomment

---
+++ config/prod.secret.exs
@@ -0,0 +1,12 @@
+use Mix.Config
+
+# In this file, we keep production configuration that
+# you likely want to automate and keep it away from
+# your version control system.
+#
+# You should document the content of this
+# file or create a script for recreating it, since it's
+# kept out of version control and might be hard to recover
+# or recreate for your teammates (or you later on).
+config :chat, Chat.Endpoint,
+  secret_key_base: "T36fp756R2SJfF2Wtfb3ZcihVQjDV5IWe87iytV+z1KdYoF9TUdoaNCysFTge4nD"
